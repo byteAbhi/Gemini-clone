6b47271bda96626ef2e7380600fcd59018b46aa1
byteAbhi <abhishekmukhiya56321@gmail.com>
2024-11-07 23:48:24 +0530

    adding all  src and public foldere

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/main'
+
+function App() {
+
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+     </>
+  )
+}
+
+export default App
